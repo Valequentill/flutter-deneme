901bbaf78cd85b245a5dc3fe66b24e4b860620b5
Valequentill <kemal__karakaya@hotmail.com>
2019-07-10 00:23:39 +0300

    githuba proje

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:tflite/tflite.dart';
+import 'package:image_cropper/image_cropper.dart';
+
+void main() => runApp(new CameraApp());
+
+class CameraApp extends StatefulWidget {
+  @override
+  _CameraAppState createState() => _CameraAppState();
+}
+
+class _CameraAppState extends State<CameraApp> {
+  File image;
+
+  picker() async {
+    print('Picker is called');
+    File img = await ImagePicker.pickImage(source: ImageSource.camera);
+    if (img != null) {
+      image = img;
+      setState(() {});
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      home: new Scaffold(
+        appBar: new AppBar(
+          backgroundColor: Colors.blueGrey[900],
+          title: new Text('                     Id Classifier'),
+        ),
+        body: new Container(
+          child: new Center(
+            child: image == null
+                ? new Text('No Image to Show ')
+                : new Image.file(image),
+          ),
+        ),
+        floatingActionButton: new FloatingActionButton(
+          onPressed: picker,
+          child: new Icon(Icons.camera_alt),
+        ),
+      ),
+    );
+  }
+}
